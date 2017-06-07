00defa59dd24597ba3ca8432b46523d4997def5b
Toby Bradshaw <tobybrad@microsoft.com>
2017-06-07 15:22:19 +0100

    Master bot app added

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-let MasterBot = require("./lib/masterBot");
+const nconf = require("nconf");
+const botbuilder = require("botbuilder");
+const MasterBot = require("./lib/MasterBot").UniversalMasterBot;
 
-module.exports = {
+var config = nconf.env().argv().file({file:'localConfig.json', search:true});
+
+function main() {
+  var masterBot = new MasterBot(new botbuilder.ChatConnector({
+    appId : config.get("MICROSOFT_APP_ID"),
+    appPassword : config.get("MICROSOFT_APP_PASSWORD")
+  }));
+  masterBot.startServer();
+
+  masterBot.dialog("/", 
+    (session, args) => {
+      session.send("masterBot: no subbot found to handle: " + session.message.text)
+    }
+  );
+
+  masterBot.on("conversationUpdate", (activity) => {
+    masterBot.send(new botbuilder.Message().address(activity.address).text("Welcome to the MasterBot"));
+  });
 }
+
+main();

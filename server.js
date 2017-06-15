4403aab853a42bc4977034c37e19059ea0f316fe
Toby Bradshaw <tobybrad@microsoft.com>
2017-06-15 04:22:54 +0100

    Enable focus tracking

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ function main() {
     appPassword : config.get("MICROSOFT_APP_PASSWORD")
   }));
   masterBot.startServer(config.get());
-  masterBot._addSubbot({ "intents":[{"type":"regex", "regex" : ".*" }],"credentials":{"type":"sharedCredentials","endpoint":"https://botlette.azurewebsites.net/api/messages","appId":"c4f802e8-dcb5-4cae-97a0-5057d8c3f307","appPassword":"kXVWvoz9W2tGqZybK8knHD1"}});
 
   masterBot.dialog("/", 
     (session, args) => {
